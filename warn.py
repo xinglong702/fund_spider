d0e327c6f6ee4aabbb10ed459756e956686ed08f
xinglongfei <71247852+xinglong702@users.noreply.github.com>
2021-05-14 16:14:50 +0800

    repair

--- warn.py
+++ warn.py
@@ -3,11 +3,13 @@ import time
 
 # 设置初始日期和当前日期
 startDate = '2021-04-26'
+startDate1 = '2012-5-12'
 # 今日日期
 endDate = time.strftime('%Y-%m-%d')
 
 # 根据日期拼出相应的excel文件路径
 startPath = 'fund数据(' + startDate + ').xls'
+startPath1 = 'fund数据(' + startDate1 + ').xls'
 endPath = 'fund数据(' + endDate + ').xls'
 
 warnList = []
@@ -19,10 +21,15 @@ def warn():
     startSheet = startBook.sheets()[0]
     startDict = dict(zip(startSheet.col_values(0, 1), startSheet.col_values(3, 1)))
 
+    startBook1 = xlrd.open_workbook(startPath1)
+    startSheet1 = startBook1.sheets()[0]
+    startDict1 = dict(zip(startSheet1.col_values(0, 1), startSheet1.col_values(3, 1)))
+
     try:
         endBook = xlrd.open_workbook(endPath)
     except Exception as e:
         return warnList
+
     endSheet = endBook.sheets()[0]
     endDict = dict(zip(endSheet.col_values(0, 1), endSheet.col_values(3, 1)))
 
@@ -31,12 +38,16 @@ def warn():
         # 防止有些自选是后面加进去的
         if fund in startDict:
             startNet = float(startDict[fund])
+            startNet1 = float(startDict1[fund])
             diff1 = (startNet - endNet) / startNet
+            diff1_1 = (startNet1 - endNet) / startNet1
             diff2 = (endNet - startNet) / startNet
             if diff1 > 0.1:
                 warnList.append(fund + '相对于' + startDate + '跌了' + str(round(diff1, 3) * 100) + '%')
             if diff1 > 0.05:
                 warnList.append(fund + '相对于' + startDate + '跌了' + str(round(diff1, 3) * 100) + '%')
+            if diff1_1 > 0.05:
+                warnList.append(fund + '相对于' + startDate1 + '跌了' + str(round(diff1_1, 3) * 100) + '%')
             if diff2 > 0.1:
                 warnList.insert(0, '恭喜基金' + fund + '相对于' + startDate + '涨了' + str(round(diff2, 4) * 100) + '%')
             if diff2 > 0.2:
